0f0730873c7772d385e7b8ebca95c8f467b6f5c8
emanuellorenzo440-collab <emanuellorenzo440@gmail.com>
2026-05-03 13:52:26 -0400

    Bootstrap frontend runtime bridge for MEL API sync

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,6 @@
 import { createMonitoringApp } from "./features/monitoring-app.js";
+import { bootstrapApiBridge, startRuntimeBridge } from "./services/mel-runtime-bridge.js";
 
+await bootstrapApiBridge();
 createMonitoringApp().start();
+startRuntimeBridge();
